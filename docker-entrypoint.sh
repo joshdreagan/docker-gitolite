621f86cfae2f7d1d25871261fd067b486d698017
Josh Reagan <josh.d.reagan@gmail.com>
2026-07-08 18:50:07 -0600

    Refactor SSH configuration to use dedicated include files for key management, authentication settings, and subsystems.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,22 +7,38 @@ if [ "${1}" = 'sshd' ]; then
 fi
 
 # Setup SSH HostKeys if needed
+conf_file='/etc/ssh/sshd_config.d/10-hostkey-overrides.conf'
+echo '# HostKey overrides' > "$conf_file"
 for algorithm in rsa ecdsa ed25519
 do
   keyfile=/etc/ssh/keys/ssh_host_${algorithm}_key
   [ -f $keyfile ] || ssh-keygen -q -N '' -f $keyfile -t $algorithm
   chmod 600 $keyfile
 
-  grep -q "HostKey $keyfile" /etc/ssh/sshd_config || echo "HostKey $keyfile" >> /etc/ssh/sshd_config
+  echo "HostKey $keyfile" >> "$conf_file"
 done
-# Disable unwanted authentications
-perl -i -pe 's/^#?((?!Password|Kerberos|GSSAPI)\w*Authentication)\s.*/\1 no/; s/^(PubkeyAuthentication) no/\1 yes/' /etc/ssh/sshd_config
-# Disable sftp subsystem
-perl -i -pe 's/^(Subsystem\ssftp\s)/#\1/' /etc/ssh/sshd_config
 
+# Disable unwanted authentications and enable wanted ones
+conf_file='/etc/ssh/sshd_config.d/20-authentication-overrides.conf'
+echo '# Authentication overrides' > "$conf_file"
+echo 'PermitRootLogin no' >> "$conf_file"
+echo 'UsePAM no' >> "$conf_file"
+echo 'HostbasedAuthentication no' >> "$conf_file"
+echo 'PasswordAuthentication no' >> "$conf_file"
+echo 'KerberosAuthentication no' >> "$conf_file"
+echo 'GSSAPIAuthentication no' >> "$conf_file"
+echo 'PubkeyAuthentication yes' >> "$conf_file"
+
+# Disable unwanted subsystems
+conf_file='/etc/ssh/sshd_config.d/50-subsystem-overrides.conf'
+echo '# Subsystem overrides' > "$conf_file"
+echo 'Subsystem     sftp     /bin/false' >> "$conf_file"
+
+# Add any additional customizations
+conf_file='/etc/ssh/sshd_config.d/90-additional-overrides.conf'
+echo '# Additional overrides' > "$conf_file"
 if [ "$SSH_DEBUG" == 'true' ]; then
-  # Set logging level
-  perl -i -pe 's/^#?(LogLevel) \w*/\1 DEBUG/' /etc/ssh/sshd_config
+  echo 'LogLevel DEBUG' >> "$conf_file"
 fi
 
 # Fix permissions at every startup
@@ -38,7 +54,7 @@ if [ ! -f ~git/.ssh/authorized_keys ]; then
   else
     echo "You need to specify SSH_KEY on first run to setup gitolite"
     echo "You can also use SSH_KEY_NAME to specify the key name (optional)"
-    echo 'Example: docker run -e SSH_KEY="$(cat ~/.ssh/id_rsa.pub)" -e SSH_KEY_NAME="$(whoami)" jgiannuzzi/gitolite'
+    echo 'Example: docker run -e SSH_KEY="$(cat ~/.ssh/id_rsa.pub)" -e SSH_KEY_NAME="$(whoami)" joshdreagan/gitolite'
     exit 1
   fi
 # Check setup at every startup
